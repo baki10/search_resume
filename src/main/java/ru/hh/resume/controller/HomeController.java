fba1fbfc588bae65ee941cc26705e29961a70c97
Ilmir Bakirov <ilmirbakirov@gmail.com>
2017-07-16 19:55:49 +0500

    switched spring logs level to info

--- src/main/java/ru/hh/resume/controller/HomeController.java
+++ src/main/java/ru/hh/resume/controller/HomeController.java
@@ -1,7 +1,5 @@
 package ru.hh.resume.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-  private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
   @GetMapping
   public String home(Model model) {
-    logger.debug("home() ---- start");
-
-    model.addAttribute("message", "HELLO WORLD!!!(from controller)");
-
-    logger.debug("home() ---- end");
-
     return "resume-list";
   }
 }
